50e8a66ff4af64906df92360ae1917076d044661
Sujal Tamrakar <82257916+sujal03@users.noreply.github.com>
2024-08-12 17:51:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st 
+from utils import generate_script
+
+# Applying Styling
+st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: #0099ff;
+    color:#ffffff;
+}
+div.stButton > button:hover {
+    background-color: #00ff00;
+    color:#FFFFFF;
+    }
+</style>""", unsafe_allow_html=True)
+
+
+st.title('❤️ YouTube Script Writing Tool') 
+
+
+# Captures User Inputs
+prompt = st.text_input('Please provide the topic of the video',key="prompt")  
+video_length = st.text_input('Expected Video Length 🕒 (in minutes)',key="video_length")  
+creativity = st.slider('Creativity limit ✨ - (0 LOW || 1 HIGH)', 0.0, 1.0, 0.2,step=0.1)
+
+submit = st.button("Generate Script for me")
+
+
+if submit:
+
+    search_result,title,script = generate_script(prompt,video_length,creativity,st.session_state['API_Key'])
+    #Let's generate the script
+    st.success('Hope you like this script ❤️')
+    #Display Title
+    st.subheader("Title:🔥")
+    st.write(title)
+    #Display Video Script
+    st.subheader("Your Video Script:📝")
+    st.write(script)
+    #Display Search Engine Result
+    st.subheader("Check Out - DuckDuckGo Search:🔍")
+    with st.expander('Show me 👀'): 
+        st.info(search_result)
